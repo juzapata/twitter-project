ef3f74eede23a7e25924cd6a0afe6aacad400607
Julia Zapata <juliamzapata@gmail.com>
2018-09-08 18:55:41 -0300

    Version 0.0.1

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
     $("#button-galinha").click(function(event){
       event.preventDefault();
       var inputValue = $("#galinha-input").val();
-      $("#posts-area").append(`<div class="dudu">${inputValue}</div>`)
-      $(".dudu").addClass("post-box");
+      $("#posts-area").append(`<div class="box-content">${inputValue}</div>`)
+      $(".box-content").addClass("post-box");
+      $("#galinha-input").val("");
+      
     });
 
     // $(".post-list").append(`<li><span>${text}</span><button class="delete-button" data-id="${key}">Excluir</button></li>`)
 
-
+// fazer o que tem no bot
 
 });
