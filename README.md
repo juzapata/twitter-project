5e91467fdfbf54497d490c4f2a6da50ff39d0984
Julia Zapata <juliamzapata@gmail.com>
2018-11-06 10:30:37 -0200

    Algumas melhoras no estilo e implementação do README

---
+++ README.md
@@ -0,0 +1,26 @@
+# Desafio Twitter v.1.0.0
+
+**Neste desafio foi desenvolvido o que seria uma cópia do Twitter** 
+
+## Versão 1.0.0
+
+- Postagem de comentários
+- Contagem do número de caracteres inseridos dentro do input.
+- Se passar dos 140 caracteres há a desativação do botão de "tweet".
+- Se passar dos 120 caracteres, o contador muda para a cor amarela.
+- Se passar dos 130 caracteres, o contador muda para a cor vermelha.
+- Ao pressionar enter (/n), no input, há o aumento da área de texto de acordo com o tamanho do texto.
+- Hora em que o tweet foi publicado no formato de 24 horas hh:mm.
+
+## Roadmap oficial do projeto
+
+## Versão 1.0.1
+- Criação de usuário.
+- Posts dos usuários serão salvos.
+- Usuário poderá excluir e/ou editar seus próprios posts.
+
+## Versão 1.0.2
+- Usuários poderão seguir outros usuários.
+- Usuários poderão dar "like" em posts de outros usuários.
+
+
--- app.js
+++ app.js
@@ -1,59 +1,55 @@
 $(document).ready(function(){
-   $("#tweet-button").on("click", function(event){
+  $("#tweet-button").on("click", function(event){
     event.preventDefault();
-    let textValue = $("#text-area").val();
-    $("#posts-area").append(`<div class="inside-posts bg-light">${textValue}</div>`);
+    let data = new Date();
+    $("#posts-area").append(`
+    <div>
+      <div class="inside-posts bg-light">${$("#text-area").val()}</div>
+      <div class="time-box bg-light">${data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds() + " de " + data.getDate() + '/' + (data.getMonth()+1) + '/' + data.getFullYear()}</div>
+    </div>`);
     $(".inside-posts").addClass("post-box");
     $("#text-area").val("");
   });
-  function areaResize(){
-    var textAreaResize = $("#text-area");
-    console.log(typeof(textAreaResize.length));
+  function inputReaResize(){
+    let textAreaResize = $("#text-area");
     for (var i = 0; i < textAreaResize.length; i++) {
     textAreaResize[i].setAttribute('style', 'height:' + (textAreaResize[i].scrollHeight) + 'px;overflow-y:hidden;');
     textAreaResize[i].addEventListener("input", OnInput, false);
-   }
-   function OnInput() {
-      this.style.height = 'auto';
-      this.style.height = (this.scrollHeight) + 'px';
-    }
-   }
-   areaResize();
+  }
+  function OnInput() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+  }
+}
+inputReaResize();
 $("#text-area").on("input keydown", function () {
     var maxWords = 139;
-    var textLength = $(this).val().length;
+    let textLength = $(this).val().length;
     // var showedWords = maxWords - textLength;
     $("#carac-count").html(textLength);
     if (textLength >= 0 && textLength < 120){
-      console.log("menor que 120")
       $("#carac-count").addClass("blue-font");
     }
     if (textLength >= 120 && textLength <= 130){
-      console.log("entre 120 e 130")
       $("#carac-count").removeClass("blue-font");
       $("#carac-count").removeClass("red-font");
       $("#carac-count").addClass("yellow-font");
     } 
     if (textLength > 130){
-      console.log("maior que 130")
       $("#carac-count").removeClass("yellow-font");
       $("#carac-count").addClass("red-font");
     }
-  });
+});
 $("#text-area").on("input keyup", function () {
   console.log("Ola: " + $(this).val().length);
   if ($(this).val() === "" ){
-    console.log("vazio");
     $("#tweet-button").attr("disabled", "disabled"); 
   } else if($(this).val().length > 140){
-    console.log("maior que 140 caracteres");
-    
     $("#tweet-button").attr("disabled", "disabled"); 
-    } else {
-      $("#tweet-button").removeAttr("disabled");
-    }
-  });
-  
+  } else {
+    $("#tweet-button").removeAttr("disabled");
+  }
+});
 });
 
  
